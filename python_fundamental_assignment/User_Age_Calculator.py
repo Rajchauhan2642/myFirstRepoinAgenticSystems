57728bab5005667e1e1519146eadc51447683e28
Divyansh Rajshekhar <raj7088487096@gmail.com>
2026-03-11 21:28:58 +0530

    User_Age_Calculator

---
+++ python_fundamental_assignment/User_Age_Calculator.py
@@ -0,0 +1,10 @@
+birth_year = input("Enter your birth year: ")
+
+birth_year = int(birth_year)
+
+current_year = 2026
+
+age = current_year - birth_year
+
+# Print the age
+print("Your age is:", age)

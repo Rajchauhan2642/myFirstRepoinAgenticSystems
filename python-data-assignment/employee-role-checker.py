3654284a4b595ec24b9132dfe48b872bb310d7f6
Divyansh Rajshekhar <raj7088487096@gmail.com>
2026-03-11 23:06:52 +0530

    employee role

---
+++ python-data-assignment/employee-role-checker.py
@@ -0,0 +1,13 @@
+employee = (101, "Ravi Kumar", "IT")
+
+roles = {"editor", "viewer", "admin"}
+
+print("----- Employee Information -----")
+print("Employee ID:", employee[0])
+print("Employee Name:", employee[1])
+print("Department:", employee[2])
+
+if "admin" in roles:
+    print("Admin Access: Yes")
+else:
+    print("Admin Access: No")
